050bfb3de1d819b14882558fb87101e6740e3ecb
Ajmal Hussain <ajmalbaltisitani229@gmail.com>
2026-07-21 04:42:34 +0500

    fix smtp connection

--- backend/controler/index.js
+++ backend/controler/index.js
@@ -8,7 +8,9 @@ const jwt = require('jsonwebtoken')
 
 
 const transporter = nodemailer.createTransport({
-    service : 'gmail',
+    host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
     
     auth:{
         user: process.env.EMAIL_USER,
