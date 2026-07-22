357f2d20391aac5ec751753ecdd3ebc0b965a786
Ajmal Hussain <ajmalbaltisitani229@gmail.com>
2026-07-22 16:38:41 +0500

    fix navigate error

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,11 +4,11 @@ const userschema = mongoose.Schema({
     name: String,
     email: {type : String , required: true , unique : true},
     password: {type : String , required : true},
-    otp : String,
     isvarified : {
         type : Boolean,
         default : false
-    }
+    },
+    otp: String
 })
 
 const user = mongoose.model("user" , userschema)
