a696121217f6a24cf010715f3c8444160140d3e3
Ajmal Hussain <ajmalbaltisitani229@gmail.com>
2026-07-23 03:49:31 +0500

    fix transpoter issue

--- backend/controler/index.js
+++ backend/controler/index.js
@@ -10,7 +10,12 @@ const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
   port: 465,
   secure: true,
-  family: 4,
+  
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+
+  
     auth:{
         user: process.env.EMAIL_USER,
         pass : process.env.EMAIL_PASS
